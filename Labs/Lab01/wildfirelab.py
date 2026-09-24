67c51f3846473b304df4e736cb228d339f3825c6
makaylapatrick <makayla.mail@gmail.com>
2026-09-24 10:58:16 -0400

    do not accidentally delete

---
+++ Labs/Lab01/wildfirelab.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+
+#Keeping track of what needs to happen in the lab
+'''
+What is actually happening?
+    step 1: need to create a 3x3 forest with a
+    buffer around the outside
+    step 2: need to set the center of forest to be burning
+    step 3: need to roll a dice to see where the fire will
+    spread over a series of specific time steps, but it will
+    not spread diagonally
+    step 4: check how the model runs through visualizing
+'''
+
+#Section 1: Libraries
+
+#The start of the script with a description of the purpose and
+# a main place where libraries can be imported.
+
+'''
+This file contains tools and scripts for completing Lab 1 for CLaSP410.
+To reproduce the plots shown in the lab report, follow the steps below!
+
+All instances of '# %%' are merely to divide the code into cells for each part.
+The goal was to create ease when debugging individual sections of the cocde.
+'''
+import numpy as np
+import matplotlib.pyplot as plt 
+import matplotlib.gridspec as grid
+import scipy as sp
+from matplotlib.colors import ListedColormap #creating a custom color map
+
+#Section 2: Defining Variables
+
+#Define constants for the different states of the forest
+burnt_bare = 1 #depicted as a tan color in the figures to show bare ground
+alive_forest = 2 #depicted as a green color in the figures to show trees
+on_fire = 3 #depicted as a red color in the figures to show flames
+
+p_spread = 1.0 # Chance to spread to adjacent cells.
+p_bare = 0.2 # Chance of cell to start as a bare patch.
+p_ignite = 0.05 # Chance of cell to start on fire.
+
+#Implement time as a varaible for the model
+time_step = 0
+
+#Section 3: Original Forest
+
+#Create a forest 
+
+nx, ny = 5, 7 # Number of cells in X and Y direction
+
+#a, b = 2, 3 # Grid cell for initial burning
+
+#creating the main forest array that the rest of the model will build from
+ori_forest = np.zeros([ny, nx]) + 2
+
+#setting the outside grid to be 1, so the fire will not spread into the ghost cells
+
+ori_forest[[ny-1], :] = 1 # Selecting the bottom row of the forest grid
+ori_forest[:, [nx-1]] = 1 # Selecting the last column of the forest grid
+ori_forest[0, :] = 1 # Selecting the top row of the forest grid
+ori_forest[:, 0] = 1 # Selecting the first column of the forest grid
+
+print(ori_forest)
+
+#Section 4: Begin the Burning
+
+#Randomly generate forest with bare and forested cells
+for i in range(1,nx-1): #Set range to avoid ghost nodes
+    for j in range(1,ny-1):
+        # Roll our "dice" to see if we get a bare spot:
+        if np.random.rand() < p_bare:
+            ori_forest[j, i] = burnt_bare # Bare spot randomly generated
+        elif np.random.rand() < p_ignite:
+            ori_forest[j,i] = on_fire # Burning spot randomly generated
+
+#Create a copy of the original forest to begin burning
+
+#ori_forest[a,b] = on_fire
+
+burn_forest = np.copy(ori_forest)
+
+print(burn_forest)
+
+#Section 5: Spread the fire
+
+'''
+Create loop to begin rolling the dice to see where the randomly generated forest
+will catch on fire and begin burning
+'''
+
+for k in range(0, time_step):
+    for i in range(1,nx-1): #Set range to avoid ghost nodes
+        for j in range(1,ny-1):
+            print(f"Current Position: {j, i}")
+            # Roll the dice to see if the fire will spread
+            if ori_forest[j,i] == on_fire:
+                if np.random.rand() < p_spread:
+                    if ori_forest[j,i+1] == alive_forest:
+                        burn_forest[j,i+1] = on_fire
+                        print("Spreading!!")
+                if np.random.rand() < p_spread:
+                    if ori_forest[j+1,i] == alive_forest:
+                        burn_forest[j+1, i] = on_fire
+                        print("Spreading!!")
+                if np.random.rand() < p_spread:
+                    if ori_forest[j,i-1] == alive_forest:
+                        burn_forest[j,i-1] = on_fire
+                        print("Spreading!!")
+                if np.random.rand() < p_spread:
+                    if ori_forest[j-1,i] == alive_forest:
+                        burn_forest[j-1,i] = on_fire
+                        print("Spreading!!")
+
+                burn_forest[j,i] = 1
+                        
+    ori_forest = np.copy(burn_forest)
+   
+    print(ori_forest)
+
+
+#Section 6: Start visualizing the model
+
+# Generate our custom segmented color map for this project.
+# We can specify colors by names and then
+# create a colormap that only uses those names. We have 3 fundamental
+# states, so we want only 3 colors.
+# Color info:
+#https://matplotlib.org/stable/gallery/color/named_colors.html
+forest_cmap = ListedColormap(['tan', 'darkgreen', 'firebrick'])
+
+# Create figure and set of axes:
+fig, ax = plt.subplots(1,1, figsize=(5,5))
+plt.axhline(0, color='k', linestyle='--', linewidth=0.7)
+plt.title(f"Forest Fire Spread Model Iteration {time_step}")
+
+# Given our "forest" object, a 2D array that contains
+# numbers 1, 2, or 3,
+# Plot this using the "pcolor" method. Be sure to use our color map and
+# set both *vmin* and *vmax*:
+ax.pcolor(ori_forest, cmap=forest_cmap, vmin=1, vmax=3)
+
+plt.show()
+
---
+++ coffee/eulerhw.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+'''
+For for this homework assignment we are trying to figure out how to calculate
+how long it takes for a coffee cup to cool to the temperature we desire.
+We use Euler's Method to run the caluclation within this code.
+
+The follow is my pseudo-code of my plan:
+1. Define what we know
+
+temp_init = initial temperature (145)
+
+temp_env =environmental temperature (18)
+
+temp_final = final drinking temperature (80)
+
+cons_k = value of cooling constant k (0.1)
+
+delta_t = estimated time step (10)
+
+tolerance = |T_curr -Ts|
+
+2. Define what we don't know
+the value of t at future time step when coffee temperature is equal to the final temperature
+3. What can we do?
+There would be an if/else loop through time steps while the temperature is greater than the final. 
+It ends when final coffee temp is reached.
+But the temperature will never reach exactly the final so you could define a tolerance
+within an absolute value or a range of absolute values the final temperature can be accepted at
+Need to create a copy of initial copy cup labeled like: new_cup
+the loop will append to the copy until the final temperature is reached
+the loop will have a statement that tests to see if the desired final temp range has been reached.
+if it is not the loop will go through an additional statement that calculates:
+
+T_new = T_curr - delta_t*cons_k*(T_curr - temp_env)
+
+4. Visualize the data
+The final step will be to plot the data as a curve so we can see how the coffee cools over time
+'''
+
+#Section 1: Import Libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Section 2: Define Variables
+
+temp_init = 145     #The initial temperature value for the coffee
+temp_env = 22       #The environmental temperature value
+temp_final = 80     #The final desired temperature of the coffee
+
+cons_k = 0.1        #A constant inlcuded in Newtons Law of Cooling Eqn.
+
+time_step = 1       #The initial time step
+delta_t = 10        #The change in timesteps?
+
+#Section 3: Make a function for the cooling equation
+
+def cooling_eqn():
+
+
+
+
+
+#within a if and else statment
+t_curr = #copy of initial
+
+t_new = #equation
+
+t_curr = #copy of new 
+
+
+
